975c8cd509d868f2d77a5dabdc0640387662fdc0
jyoti-varshney7 <jyoti.varshney_cs18@gla.ac.in>
2020-05-27 18:29:36 +0530

    Initial commit

---
+++ app/src/main/java/e/meghna/informativeapp/MainActivity.java
@@ -0,0 +1,45 @@
+package e.meghna.informativeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.NumberPicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    NumberPicker possibilities;
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        possibilities = (NumberPicker) findViewById(R.id.numberPicker);
+        webView = (WebView) findViewById(R.id.webView);
+        String[] possibilitiesString = {
+                "Android",
+                "Checklist text-input fields" ,
+                "Coursera",
+                "Supelec"};
+        possibilities.setDisplayedValues(possibilitiesString);
+        possibilities.setMinValue(0);
+        possibilities.setMaxValue(possibilitiesString.length-1);
+    }
+    public void navigate(View v){
+        int choice = possibilities.getValue();
+        if(choice == 0){
+            webView.setWebViewClient(new WebViewClient());
+            webView.loadUrl("file:///android_assert/android.html");}
+        else if(choice == 1)
+            webView.loadUrl("file:///android_assert/checklist.html");
+        else if(choice == 2)
+            webView.loadUrl("http://www.coursera.org");
+        else if(choice == 3)
+            webView.loadUrl("file:///android_assert/supelec.html");
+
+    }
+    }
+
